807ac10c8e923e51cfedd8d3574489fec4e24b13
Emmanuel Tolulope <emmany4567@gmail.com>
2021-11-27 22:03:07 +0100

    First Digital CLock using more Js inbuilt functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function tellTime(){
+            var date = new Date();
+            var h = date.getHours(); // returns 0-23
+            var m = date.getMinutes(); //returns 0-59
+            var s = date.getSeconds(); //returns 0 -59
+            var session = "AM";
+            
+            if(h == 0){
+                h = 12;
+            }
+            if(h > 12){
+                h = h - 12;
+                session = "PM";
+            }
+            
+            h = (h > 10)? "0" + h : h;
+            m = (m < 10)? "0" + m : m;
+            s = (s < 10)? "0" + s : s;
+            var time = h + ":" + m + ":" + s + " " + session;
+            document.getElementById("clockDisplayPoint").innerText = time;
+            setTimeout(tellTime, 1000);
+        }
+            tellTime();
+        
